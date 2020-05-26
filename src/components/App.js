1f2b35422211a806920ae55fb6ab7cb22c864ab6
Tom Kota <63372913+tom2kota@users.noreply.github.com>
2020-05-27 00:51:05 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -29,54 +29,25 @@ class App extends Component {
                         </h2>
                     </div>
 
-                    <div className="ui compact menu">
-                        <div className="ui simple dropdown item">
-                            Dropdown
-                            <i className="dropdown icon"/>
-                            <div className="menu">
-                                <div className="item">Choice 1</div>
-                                <div className="item">Choice 2</div>
-                                <div className="item">Choice 3</div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="ui floating dropdown labeled search icon button">
-                        <i className="world icon"></i>
-                        <span className="text">Select Language</span>
-                        <div className="menu">
-                            <div className="item">Arabic</div>
-                            <div className="item">Chinese</div>
-                            <div className="item">Danish</div>
-                            <div className="item">Dutch</div>
-                            <div className="item">English</div>
-                            <div className="item">French</div>
-                            <div className="item">German</div>
-                            <div className="item">Turkish</div>
-                            <div className="item">Vietnamese</div>
-                        </div>
-                    </div>
-
 
                     <div className="right menu">
 
-
-                        <div className="ui selection dropdown">
-                            <input type="hidden" name="language"/>
-                            <i className="dropdown icon"/>
-                            <div className="default text">Language</div>
-                            <div className="menu">
-                                <div className="item" data-value="1">
-                                    English &nbsp;
-                                    <i className="flag gb" onClick={() => this.onLanguageChange('English')}/>
-                                </div>
-                                <div className="item" data-value="0">
-                                    Netherlandish &emsp;
-                                    <i className="flag nl" onClick={() => this.onLanguageChange('Netherlandish')}/>
+                        <div className="ui compact menu">
+                            <div className="ui simple dropdown item">
+                                Select Language
+                                <i className="dropdown icon"/>
+                                <div className="menu">
+                                    <div className="item" data-value="1">
+                                        English &nbsp;
+                                        <i className="flag gb" onClick={() => this.onLanguageChange('English')}/>
+                                    </div>
+                                    <div className="item" data-value="0">
+                                        Netherlandish &emsp;
+                                        <i className="flag nl" onClick={() => this.onLanguageChange('Netherlandish')}/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-
 
                         <div className="item"> Select a Language:</div>
                         <div className="item" onClick={() => this.onLanguageChange('English')}>
